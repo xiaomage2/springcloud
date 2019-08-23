bcae54ac762304cae7442968a37da64064ee4bdd
mayunning <408218852@qq.com>
2019-08-23 15:01:23 +0800

    springcloud  版本冲突问题处理

--- eureka/src/main/java/com/xmg/eureka/EurekaApplication.java
+++ eureka/src/main/java/com/xmg/eureka/EurekaApplication.java
@@ -2,7 +2,9 @@ package com.xmg.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer//开启服务注册中心，开启以后表示该应用是一个服务注册中心
 @SpringBootApplication
 public class EurekaApplication {
 
