d0559ba02dac653db0e085bbb837d0a531e00fa5
mayunning <408218852@qq.com>
2019-08-23 23:33:45 +0800

    复制 provider 成consumer，修改配置

---
+++ consumer/src/main/java/com/xmg/consumer/ConsumerApplication.java
@@ -0,0 +1,15 @@
+package com.xmg.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class ConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
+
+}
