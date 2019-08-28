d0187b74e50a182dc808e768caf40e16c5dda5f9
mayunning <408218852@qq.com>
2019-08-28 13:57:39 +0800

    公共模块

---
+++ commons/src/main/java/com/xmg/commons/entity/ScenceEntity.java
@@ -0,0 +1,88 @@
+package com.xmg.commons.entity;
+
+
+public class ScenceEntity {
+    private Integer id;
+    private String name;
+    private String paramKey;
+    private String dbtype;
+    private String dbname;
+    private String scence;
+    private String columnDesc;
+    private String clientid;
+    private String desc;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getParamKey() {
+        return paramKey;
+    }
+
+    public void setParamKey(String paramKey) {
+        this.paramKey = paramKey;
+    }
+
+    public String getDbtype() {
+        return dbtype;
+    }
+
+    public void setDbtype(String dbtype) {
+        this.dbtype = dbtype;
+    }
+
+    public String getDbname() {
+        return dbname;
+    }
+
+    public void setDbname(String dbname) {
+        this.dbname = dbname;
+    }
+
+    public String getScence() {
+        return scence;
+    }
+
+    public void setScence(String scence) {
+        this.scence = scence;
+    }
+
+    public String getColumnDesc() {
+        return columnDesc;
+    }
+
+    public void setColumnDesc(String columnDesc) {
+        this.columnDesc = columnDesc;
+    }
+
+    public String getClientid() {
+        return clientid;
+    }
+
+    public void setClientid(String clientid) {
+        this.clientid = clientid;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+}
+
