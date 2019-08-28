c5ffff976f5e911b12c146a054e0c018ebdd8544
mayunning <408218852@qq.com>
2019-08-28 13:58:49 +0800

    引入公共模块，和链接数据库相关的的jar 以及配置文件

--- consumer/src/main/java/com/xmg/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/xmg/consumer/ConsumerApplication.java
@@ -2,7 +2,10 @@ package com.xmg.consumer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient
 @SpringBootApplication
@@ -12,4 +15,13 @@ public class ConsumerApplication {
         SpringApplication.run(ConsumerApplication.class, args);
     }
 
+    /**
+     * RestTemplate 是spring-web 这个jar里面的东西，它封装了http请求
+     * @return
+     */
+    @LoadBalanced//开启负载均衡
+    @Bean
+    RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 }
